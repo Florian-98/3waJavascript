52c6ab6e97a95a14e70aeb919eb4fbc8b96c0cf6
Florian <floflogobin@gmail.com>
2020-02-13 10:07:38 +0100

    jour 7 fin javascript

--- Developpement/Javascript/Jour 6/index.js
+++ Developpement/Javascript/Jour 6/index.js
@@ -5,22 +5,66 @@ let form = document.querySelector('form');
 let name = document.getElementById("name");
 let firstName = document.getElementById("fisrtName");
 let age = document.getElementById("age");
+
+
 form.addEventListener("submit", function(event) {
   event.preventDefault();
-  console.log(name + firstName + age);
+
+  const p = document.createElement('p');
+
+  p.innerText = name.value;
+
+  const z = document.createElement('p');
+
+  z.innerText = age.value;
+
+
+
+
+  form.appendChild(p);
+  form.appendChild(z);
+
+});
+
+//lien//
+
+let lien = document.querySelector('a');
+lien.addEventListener("click", function(event) {
+
+  event.preventDefault();
 });
 
 
 
 
 
-/* let object = {
-  fruit: orange,
-  legume: haricot,
-  fruitEtLegume : function() {
-    return this.fruit + this.legume;
+
+
+//objet//
+
+let voiture = {
+  pneu : "michelin",
+  moteur: "v12",
+  getCarEngine: function () {
+    this.fabricant = 'fiat';
+    this.modele = 'panda';
+    this.annee = 2006;
+    return {
+      fabricant: this.fabricant,
+      model: this.modele,
+      annee: this.annee
+    };
+
+    console.log(this);
+  },
+  getOriginalCar: function() {
+    return {
+      pneu: this.pneu,
+      moteur: this.moteur
+    }
   }
 }
-console.log(object.fruitEtLegume);
 
-*/
+
+//voiture = voiture.getOriginalCar();
+console.table(voiture);
